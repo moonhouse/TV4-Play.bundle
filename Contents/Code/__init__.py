c591b81f9c64058d3ca33cac5c5ef5fa6f49b05e
sander1 <ple@se.not>
2010-09-17 09:03:48 +0800

    Small changes for v2 framework, icons and some var names

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -36,12 +36,13 @@ def Start():
   MediaContainer.art = R(PLUGIN_ARTWORK)
 
   # Set the default cache time
-  HTTP.SetCacheTime(CACHE_INTERVAL)
+  HTTP.CacheTime = CACHE_INTERVAL
+  HTTP.Headers['User-agent'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.10) Gecko/20100914 Firefox/3.6.10'
 
 ###################################################################################################
 
 def UpdateCache():
-  HTTP.Request(PROGRAMS_XML, cacheTime=CACHE_INTERVAL)
+  HTTP.Request(PROGRAMS_XML, cacheTime=CACHE_INTERVAL).content
 
 ###################################################################################################
 
@@ -51,53 +52,50 @@ def MainMenu():
   categories = XML.ElementFromURL(PROGRAMS_XML, encoding='utf-8', errors='ignore').xpath('/v:xml/v:category/v:subcategories/v:category[@level="1"]', namespaces=NS_VIDEOAPI)
   for category in categories:
     name = category.get('name')
-    thumb = getThumb(name)
-    dir.Append(Function(DirectoryItem(TV4Programs, title=name, thumb=thumb), title=name, thumb=thumb))
+    dir.Append(Function(DirectoryItem(TV4Programs, title=name, thumb=Function(GetThumb, name=name)), title=name, thumb=name))
 
   return dir
 
 ####################################################################################################
 
-def TV4Programs(sender, title, thumb, useXml=True, programId=None):
+def TV4Programs(sender, title, thumb, use_xml=True, program_id=None):
   dir = MediaContainer(title2=title)
 
   # Use the 'main' XML file (www.tv4play.se/?view=xml) to display tv programs
   # This function (TV4Programs) is also used if we're deeper in the website and stumble upon an XML file that contains empty nodes for an extra level of 'subcategories'
-  if useXml:
+  if use_xml:
     programs = XML.ElementFromURL(PROGRAMS_XML, encoding='utf-8', errors='ignore').xpath('/v:xml/v:category/v:subcategories/v:category[@name="'+title+'"]/v:subcategories/v:category[@level="2"]', namespaces=NS_VIDEOAPI)
     for program in programs:
       name = program.get('name')
-      thumb = getThumb(name, thumb)
       # We need the id of this program, unfortunately this isn't available in the XML file and we need to grab it from the program's home page
       # We don't do this here, since we only need the id if a user selects this tv program. Otherwise we'd have to loop over all the programs, which would increase load times
       url = program.xpath('./v:views/v:view/v:url', namespaces=NS_VIDEOAPI)[0].text
       url = re.findall('^(.+?)\?view=xml', url)[0]
-      dir.Append(Function(DirectoryItem(TV4Views, title=name, thumb=thumb), title=name, url=url, thumb=thumb))
+      dir.Append(Function(DirectoryItem(TV4Views, title=name, thumb=Function(GetThumb, name=name, parent=thumb)), title=name, url=url, thumb=thumb))
   else:
-    content = HTTP.Request( PROGRAMS_HTML % programId ).content
+    content = HTTP.Request( PROGRAMS_HTML % program_id ).content
     programs = XML.ElementFromString(content).xpath('//ul/li/div/p/a')
     for program in programs:
       # Cleanup the name
       name = program.text.strip().title()
-      thumb = getThumb(name, thumb)
       id = program.get('href')
       id = re.findall('^/(.+?)\?ajax', id)[0]
       url = PROGRAM_VIEWS_XML % id
-      dir.Append(Function(DirectoryItem(TV4Views, title=name, thumb=thumb), title=name, url=url, thumb=thumb, lookupId=False))
+      dir.Append(Function(DirectoryItem(TV4Views, title=name, thumb=Function(GetThumb, name=name, parent=thumb)), title=name, url=url, thumb=thumb, lookup_id=False))
   return dir
 
 ####################################################################################################
 
-def TV4Views(sender, title, url, thumb, lookupId=True):
+def TV4Views(sender, title, url, thumb, lookup_id=True):
   dir = MediaContainer(title2=title)
 
   # When we want to display the different views of a program, we first need to know what the id of that program is so we can download the right XML file.
   # But... we only know the program's name and url of its home page. The url is used here to find the program's id.
-  if lookupId == True:
-    programId = XML.ElementFromURL(url, isHTML=True, encoding='utf-8', errors='ignore', cacheTime=CACHE_INTERVAL_LONG).xpath('/html/body//div[@id="browser"]//ul[@class="breadcrumbs"]//li[last()]/h3/a')[0]
-    programId = programId.get('href')
-    programId = re.findall('browser=([0-9\.]+)', programId)[0]
-    url = PROGRAM_VIEWS_XML % programId
+  if lookup_id == True:
+    program_id = HTML.ElementFromURL(url, encoding='utf-8', errors='ignore', cacheTime=CACHE_INTERVAL_LONG).xpath('/html/body//div[@id="browser"]//ul[@class="breadcrumbs"]//li[last()]/h3/a')[0]
+    program_id = program_id.get('href')
+    program_id = re.findall('browser=([0-9\.]+)', program_id)[0]
+    url = PROGRAM_VIEWS_XML % program_id
 
   # Get the page content first with HTTP.Request so we can check if we get something back
   content = HTTP.Request(url, cacheTime=CACHE_INTERVAL_LONG).content
@@ -117,19 +115,17 @@ def TV4Views(sender, title, url, thumb, lookupId=True):
           if name.isupper() and url.find('keywords') != -1:
             name = name.title()
 
-          thumb = getThumb(name, thumb)
-
           # If the type of view is 'cliplist', the linked XML file contains videos
           if kind == 'cliplist':
-            dir.Append(Function(DirectoryItem(TV4Videos, title=name, thumb=thumb), title=name, url=url))
+            dir.Append(Function(DirectoryItem(TV4Videos, title=name, thumb=Function(GetThumb, name=name, parent=thumb)), title=name, url=url))
           # If the type of view is categorylist, we have to iterate at least onces more over the contents of the linked XML file
           # In this case we don't need to look for an id, instead we can use the provided link (which contains an id)
           elif kind == 'categorylist':
-            dir.Append(Function(DirectoryItem(TV4Views, title=name, thumb=thumb), title=name, url=url, thumb=thumb, lookupId=False))
+            dir.Append(Function(DirectoryItem(TV4Views, title=name, thumb=Function(GetThumb, name=name, parent=thumb)), title=name, url=url, thumb=thumb, lookup_id=False))
       else:
         # If the nodes do not contain any childs, try to use information from the piece of HTML code that's used on the website in XHR
         # This means that there's probably an extra level of subcategories
-        dir = TV4Programs(sender, title, thumb, useXml=False, programId=programId)
+        dir = TV4Programs(sender, title, thumb, use_xml=False, program_id=program_id)
 
   return dir
 
@@ -165,24 +161,26 @@ def TV4Videos(sender, title, url, page=1):
 
 ####################################################################################################
 
-def getThumb(name, parent=None):
+def GetThumb(name=None, parent=None):
   if name == 'Aktualitet':
-    return R('icon-Aktualitet.png')
+    return Redirect( R('icon-Aktualitet.png') )
   elif name == 'Hem & fritid':
-    return R('icon-HemOchFritid.png')
+    return Redirect( R('icon-HemOchFritid.png') )
   elif name == 'Nyheter':
-    return R('icon-Nyheter.png')
+    return Redirect( R('icon-Nyheter.png') )
   elif name == 'Nöje & humor':
-    return R('icon-NojeOchHumor.png')
+    return Redirect( R('icon-NojeOchHumor.png') )
   elif name == 'Sport':
-    return R('icon-Sport.png')
+    return Redirect( R('icon-Sport.png') )
   elif name == 'Fotbollskanalen':
-    return R('icon-Fotbollskanalen.png')
+    return Redirect( R('icon-Fotbollskanalen.png') )
   elif name == 'Hockeykanalen':
-    return R('icon-Hockeykanalen.png')
+    return Redirect( R('icon-Hockeykanalen.png') )
   elif name == 'Lattjo lajban':
-    return R('icon-LattjoLajban.png')
-  elif parent == None:
-    return R(PLUGIN_ICON_DEFAULT)
+    return Redirect( R('icon-LattjoLajban.png') )
+  elif name == 'Barn':
+    return Redirect( R(PLUGIN_ICON_DEFAULT) )
+  elif parent != None:
+    return GetThumb(name=parent)
   else:
-    return parent
+    return Redirect( R(PLUGIN_ICON_DEFAULT) )
